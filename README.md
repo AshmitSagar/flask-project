28dcfc7154f000c536e83029cf7ce2fd6b80f662
Ashmit <sagarashmit@gmail.com>
2025-09-20 15:46:42 +0530

    add templates folder and include navbar in homepage

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ Fully functional flask driven webapp
 
 To run this code
 ``` python
-flask --app market.py run --debug
+flask --app "market.py" run --debug
 ```
+
+As was seen in the last lecture we hard coded the html within the python code itself.
+This approach is very diffcult to maintain and should be avoided.
+Rather we make templates which help us maintain separation between html/css and python code
--- market.py
+++ market.py
@@ -1,23 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-# root page
 @app.route("/")
-def hello_world():
-    return "<h1>Hello World</h1>"
-
-
-"""
-# static about page
-@app.route("/about")
-def about_page():
-    return "<h1>About Page</h1>"
-"""
-
-
-# Dynamic routes
-@app.route("/about/<username>")
-def about_page(username):
-    return f"<h1>This is the about page of {username}</h1>"
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
