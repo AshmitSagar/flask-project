54ed8f33393c1549bd6b3b5f368e5ddf77429b9e
Ashmit <sagarashmit@gmail.com>
2025-09-21 03:06:46 +0530

    add flask form and create register page

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "48cc2c6d8601f35a82eb8e4e"
 db = SQLAlchemy(app)
 
 from market import routes
